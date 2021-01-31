9979db064829fc837253ac07291411b09aad1a29
Rafal Pajdak <rpajdak@gmail.com>
2021-01-31 11:50:55 +0100

    Create FlightNumber model.

---
+++ src/main/java/com/codeccol/flights/model/FlightNumber.java
@@ -0,0 +1,28 @@
+package com.codeccol.flights.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "flightNumbers")
+public class FlightNumber {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+    @Column(name = "iataNumber")
+    private String iataNumber;
+    @Column(name = "icaonumber")
+    private String icaoNumber;
+    @Column(name = "number")
+    private int number;
+
+    public FlightNumber() {
+    }
+}
