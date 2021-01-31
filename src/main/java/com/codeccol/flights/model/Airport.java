757d0588f72bd8841b7ad838409cf8a8bc31d32d
Rafal Pajdak <rpajdak@gmail.com>
2021-01-31 11:44:17 +0100

    Create Airport model.

---
+++ src/main/java/com/codeccol/flights/model/Airport.java
@@ -0,0 +1,28 @@
+package com.codeccol.flights.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "airports")
+public class Airport {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    @Column(name = "iata_code")
+    private String iataCode;
+
+    @Column(name = "icao_code")
+    private String icaoCode;
+
+
+    public Airport() {
+    }
+}
