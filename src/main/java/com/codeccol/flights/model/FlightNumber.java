6e7748404505f9049e17b5f8a1696200c1291e32
Rafal Pajdak <rpajdak@gmail.com>
2021-02-02 20:42:05 +0100

    Add annotation from lombook.

--- src/main/java/com/codeccol/flights/model/FlightNumber.java
+++ src/main/java/com/codeccol/flights/model/FlightNumber.java
@@ -1,9 +1,6 @@
 package com.codeccol.flights.model;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 
