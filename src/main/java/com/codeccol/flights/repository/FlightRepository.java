b186ddeb51068c394612c419ea0c0a7832640c67
Rafal Pajdak <rpajdak@gmail.com>
2021-02-09 19:13:49 +0100

    Add method to get flight by IcaoCode and by date.

--- src/main/java/com/codeccol/flights/repository/FlightRepository.java
+++ src/main/java/com/codeccol/flights/repository/FlightRepository.java
@@ -5,16 +5,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Long> {
 
-    @Query("SELECT f.regNumber, a.iataCode from Flight flight JOIN flight.aircraft f JOIN flight.airline a " +
+    @Query("SELECT flight from Flight flight JOIN flight.aircraft f JOIN flight.airline a " +
             "WHERE flight.airline.iataCode= :iataCode")
-    List<String> getFlightByRegistration(String iataCode);
+    List<Flight> getFlightByAirline(String iataCode);
 
-    @Query("SELECT flight FROM Flight flight JOIN flight.aircraft aircraft WHERE flight.aircraft.iataCode = 'A388'")
+    @Query("SELECT flight FROM Flight flight JOIN flight.aircraft aircraft WHERE aircraft.iataCode = 'A388'")
     List<Flight> getAllA380Flights();
+
+    @Query("SELECT flight FROM Flight flight JOIN flight.flight flightumber WHERE flightumber.icaoNumber = :icaoNumber and flight.date= :date")
+    Optional<Flight> getFlightByDateAndIcaoFlightNumber(String icaoNumber, LocalDate date);
+
 
 }
